a926952f59e20230b690e9de668d1a4cbe16f3af
DutchRascal <Andre@iMac.fritz.box>
2017-08-01 16:56:44 +0200

    lesson-09

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -8,28 +8,42 @@
 
 import UIKit
 
-class LeagueVC: UIViewController {
+class LeagueVC: UIViewController
+{
+    
+    var player: Player!
 
-    override func viewDidLoad() {
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        player = Player()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onNextTapped(_ sender: UIButton)
+    {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
-    */
-
+    
+    @IBAction func onMensTapped(_ sender: UIButton)
+    {
+        selectLeague(leagueType: "mens")
+    }
+    @IBAction func onWomensTapped(_ sender: UIButton)
+    {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: UIButton)
+    {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String)
+    {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
 }
+
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Andre Boevink on 01/08/2017.
+//  Copyright © 2017 Andre Boevink. All rights reserved.
+//
+
+import Foundation
+
+struct Player
+{
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
